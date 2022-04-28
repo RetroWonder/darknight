f8c299d43bb560319ae4be768a128328e69a09bc
Gabriel M <87896375+RetroWonder@users.noreply.github.com>
2022-04-27 23:24:18 -0700

    Add files via upload

---
+++ waterworm/offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1651127003,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/iconpoint-sheet0.png",
+		"images/pu1-sheet0.png",
+		"images/tiledbackground2.png",
+		"images/logo-sheet0.png",
+		"media/bg.ogg",
+		"media/click.ogg",
+		"media/shop.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
